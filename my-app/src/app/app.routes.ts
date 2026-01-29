7581abc02c401eb3575bd968cf4d08d66fa43363
axarpatel0789 <axarpatel0789@gmail.com>
2026-01-29 11:41:13 +0000

    first

---
+++ my-app/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { UserList } from './features/user-list/user-list';
+
+export const routes: Routes = [
+    {path: '', component: UserList},
+];
---
+++ my-app/src/app/features/user-list/user-list.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { UserService } from '../../core/services/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  imports: [CommonModule],
+  templateUrl: './user-list.html',
+  styleUrl: './user-list.css',
+})
+export class UserList {
+  users: any[] = [];
+  loading = true;
+  error: string | null = null;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load users';
+        this.loading = false;
+        console.error(err);
+      }
+    });
+  }
+}
